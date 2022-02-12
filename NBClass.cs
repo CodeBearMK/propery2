de606ce4da4897cc3ee4b4f566e039e0c9f367d7
cat32tw <mike1993xd@gmail.com>
2022-02-13 01:07:40 +0800

    存取子練習2

---
+++ NBClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propery2
+{
+    internal class NBClass
+    {
+        private int price;
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0) value = 0;
+                price = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace propery2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NBClass sun = new NBClass();
+            sun.Price = 25000;
+            Console.WriteLine("sun筆電單價{0}", sun.Price);
+            Console.Read();
+        }
+    }
+}
